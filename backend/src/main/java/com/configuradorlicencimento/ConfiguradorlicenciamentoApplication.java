f64282eacb636e39f63233d6d01c872c266aa59e
raydson.ferreira <raydson.ferreira.lemaf@gmail.com>
2020-07-27 10:40:02 -0300

    criando bootstrap inicial do backend

---
+++ backend/src/main/java/com/configuradorlicencimento/ConfiguradorlicenciamentoApplication.java
@@ -0,0 +1,13 @@
+package com.configuradorlicencimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfiguradorlicenciamentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfiguradorlicenciamentoApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/configuradorlicencimento/ConfiguradorlicenciamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configuradorlicencimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfiguradorlicenciamentoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
