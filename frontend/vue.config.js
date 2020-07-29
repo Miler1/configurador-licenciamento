50be7dd1a7bbc663f3cfab0f68c7d9b90f6b42ee
raydson.ferreira <raydson.ferreira.lemaf@gmail.com>
2020-07-29 09:44:01 -0300

    terminando o bootstrap inicial do projeto

---
+++ frontend/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    devServer: {
+		proxy: 'http://localhost:9909/'
+    },
+    runtimeCompiler: true,
+    publicPath: process.env.NODE_ENV === 'production' ? '/configurador/' : '/',
+    outputDir: '../backend/src/main/resources/static',
+    lintOnSave: false
+};
