8f93c8abbf5a3293d44c03c42e0f218af710c4c3
francis.moura <francis.lemaf@gmail.com>
2020-07-31 15:11:40 -0300

    [Issue_#06] - Tela de login do configurador

---
+++ frontend/src/components/CardLogin.vue
@@ -0,0 +1,106 @@
+<template lang="pug">
+
+				v-card(class="elevation-12", id="card-login")
+								v-row(class="ma-0 pa-5", id="row-login")
+												v-col(id="col-logo", cols="12")
+																v-img(:src="require('@/assets/img/logo_configurador_licenciamento.png')"
+																				height="116px",
+																				width="310px")
+
+												v-col(class="pa-0", cols="12")
+
+																v-card-text(class="pa-0")
+																				v-form(id="form-login")
+
+																								label(class="label-login") CPF
+																								v-text-field(solo,
+																												dense,
+																												name="cpf",
+																												type="text",
+																												class="pa-0 ma-0",
+																												id="QA-btn_cpf")
+
+																												v-icon(slot="append") mdi-account
+
+																								label(class="label-login") Senha
+																								v-text-field(solo,
+																												dense,
+																												name="senha",
+																												type="password",
+																												class="pa-0 ma-0",
+																												id="QA-btn_senha")
+
+																												v-icon(slot="append") mdi-key
+
+																v-card-actions(class="pa-0")
+																				v-btn(id="QA-btn_login", width="100%")
+																								v-icon(left) mdi-login
+																								span(id="teste") Entrar
+
+												v-col(id="col-esqueci-senha", cols="12", align="start")
+																	a(id="link-esquecida-senha", href="") Esqueci minha senha
+
+</template>
+
+<script>
+
+export default {
+	name: "CardLogin"
+}
+
+</script>
+
+<style lang="less" scoped>
+				@import "../assets/css/variaveis.less";
+
+				#card-login{
+								background-color: rgba(255, 255, 255, 0.91);
+								border: 1px solid @border-components;
+								border-radius: 10px;
+								height: auto;
+								width: 350px;
+
+								#row-login {
+												flex-direction: column;
+
+												#col-logo {
+																padding: 0 0 20px 0;
+												}
+
+												#form-login {
+																align-content: start;
+																color: @text-color;
+																font-size: 14px;
+
+																.label-login {
+																				align-content: start;
+																}
+
+																.v-text-field {
+																				font-size: 16px;
+
+																				.v-icon {
+																								color: @icon-color;
+																				}
+																}
+												}
+
+												#QA-btn_login {
+																background-color: @bg-btn-login;
+																color: @btn-text-color;
+																font-size: 14px;
+																text-transform: none !important;
+												}
+
+												#col-esqueci-senha {
+																padding: 20px 0 0 0 !important;
+
+																#link-esqueci-senha {
+																				color: @info;
+																				font-size: 13px;
+																}
+												}
+								}
+				}
+
+</style>
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+
+				v-container(class="fill-height", id="container-login")
+								CardLogin
+
+</template>
+
+<script>
+import CardLogin from "../components/CardLogin"
+
+export default {
+	name: "Login",
+	components: {CardLogin},
+	props: {
+		source: String,
+	},
+	container: {
+		CardLogin
+	}
+}
+
+</script>
+
+<style lang="less" scoped>
+
+				#container-login {
+								align-content: center;
+								background: url("../assets/img/bg_login.png") no-repeat center fixed;
+								background-size: 100% 100%;
+								justify-content: center;
+								max-width: 100% !important;
+								padding: 0;
+								position: fixed;
+				}
+
+</style>
