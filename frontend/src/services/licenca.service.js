0932909fa6462e12904d27719f0afc63deb65453
kellyson.santos <kellyson.santos.lemaf@gmail.com>
2020-08-12 12:02:38 -0300

    LICENCA - Componente de listagem adicionado

--- frontend/src/services/licenca.service.js
+++ frontend/src/services/licenca.service.js
@@ -2,6 +2,20 @@ import ApiService from './api.service'
 
 export default {
 
-	salvar: async (licenca) => ApiService.post('/licenca/salvar', licenca)
+	salvar: async (licenca) => ApiService.post('/licenca/salvar', licenca),
+
+	listar: async (parametrosFiltro) => {
+		if(parametrosFiltro){
+			return ApiService.post(`licenca/lista/?
+						page=${parametrosFiltro.pagina > 0 ? parametrosFiltro.pagina : 0}
+						&size=${parametrosFiltro.itemsPorPagina > 0 ? parametrosFiltro.itemsPorPagina : 10}
+						&sort=${parametrosFiltro.tipoOrdenacao ? parametrosFiltro.tipoOrdenacao : 'dataCadastro,asc'}`,
+
+			parametrosFiltro
+			);
+		}else {
+			return ApiService.post('licenca/lista/?page=0&size=10&sort=dataCadastro,asc', {});
+		}
+	}
 
 }
