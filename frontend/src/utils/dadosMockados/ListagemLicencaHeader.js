701d6553d12f0782eef23a44cc78a4dc2cc1b54b
francis.moura <francis.lemaf@gmail.com>
2020-08-14 11:56:21 -0300

    Melhoria no nome da coluna 'Prazo de validade' da licença e esconder footer do grid de listagem quando não há dados para listar

--- frontend/src/utils/dadosMockados/ListagemLicencaHeader.js
+++ frontend/src/utils/dadosMockados/ListagemLicencaHeader.js
@@ -1,7 +1,7 @@
 const HEADER = [
 	{ text: 'Tipo', value: 'sigla', width: '175px' },
 	{ text: 'Nomenclatura', value: 'nome' },
-	{ text: 'Prazo de validade', value: 'validadeEmAnos', align: 'center' },
+	{ text: 'Prazo de validade(anos)', value: 'validadeEmAnos', align: 'center' },
 	{ text: 'Finalidade', value: 'finalidade'},
 	{ text: 'Status', value: 'ativo', width: '145px'},
 	{ text: 'Ações', value: 'actions', sortable: false, align: 'center', width: '100px'},
