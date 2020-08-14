3d176ee108790a4047c52da0645b27f0e0915e64
francis.moura <francis.lemaf@gmail.com>
2020-08-14 11:03:07 -0300

    Adição de coluna prazo validade no grid de listagem das licenças

--- frontend/src/utils/dadosMockados/ListagemLicencaHeader.js
+++ frontend/src/utils/dadosMockados/ListagemLicencaHeader.js
@@ -1,7 +1,8 @@
 const HEADER = [
 	{ text: 'Tipo', value: 'sigla', width: '175px' },
 	{ text: 'Nomenclatura', value: 'nome' },
-	{ text: 'Finalidade', value: 'finalidade', width: '170'},
+	{ text: 'Prazo de validade', value: 'validadeEmAnos', align: 'center' },
+	{ text: 'Finalidade', value: 'finalidade'},
 	{ text: 'Status', value: 'ativo', width: '145px'},
 	{ text: 'Ações', value: 'actions', sortable: false, align: 'center', width: '100px'},
 ];
