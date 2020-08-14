14560770427ea4e5ead1c55a5577df3e4c9ad92b
kellyson.santos <kellyson.santos.lemaf@gmail.com>
2020-08-14 15:38:50 -0300

    IDENTAÇÃO - String Utils

--- backend/src/main/java/com/configuradorlicenciamento/configuracao/utils/StringUtil.java
+++ backend/src/main/java/com/configuradorlicenciamento/configuracao/utils/StringUtil.java
@@ -5,61 +5,61 @@ import java.util.ArrayList;
 
 public class StringUtil {
 
-	public static Boolean vaziaOuNula(String valor) {
+    public static Boolean vaziaOuNula(String valor) {
 
-		if(valor == null || valor.isEmpty() || valor.isBlank()) {
-			return true;
-		}
+        if(valor == null || valor.isEmpty() || valor.isBlank()) {
+            return true;
+        }
 
-		return false;
+        return false;
 
-	}
+    }
 
-	public static String formataCEP(String cep) {
+    public static String formataCEP(String cep) {
 
-		cep = removeCaracteresEspeciais(cep);
+        cep = removeCaracteresEspeciais(cep);
 
-		if(cep != null && !cep.isBlank()) {
-			return cep.replaceFirst("(\\d{2})(\\d{3})(\\d+)", "$1.$2-$3");
-		}
+        if(cep != null && !cep.isBlank()) {
+            return cep.replaceFirst("(\\d{2})(\\d{3})(\\d+)", "$1.$2-$3");
+        }
 
-		return cep;
+        return cep;
 
-	}
+    }
 
-	public static String removeCaracteresEspeciais(String valor) {
+    public static String removeCaracteresEspeciais(String valor) {
 
-		if(valor != null && !valor.isBlank()) {
-			valor = valor.replaceAll("[^a-zA-Z0-9\\s+]", "");
-		}
+        if(valor != null && !valor.isBlank()) {
+            valor = valor.replaceAll("[^a-zA-Z0-9\\s+]", "");
+        }
 
-		return valor;
+        return valor;
 
-	}
+    }
 
-	public static String apenasNumeros(String valor) {
+    public static String apenasNumeros(String valor) {
 
-		if(valor != null && !valor.isBlank()) {
+        if(valor != null && !valor.isBlank()) {
 
-			valor = removeCaracteresEspeciais(valor);
-			valor = valor.replaceAll("[a-zA-Z]+", "");
+            valor = removeCaracteresEspeciais(valor);
+            valor = valor.replaceAll("[a-zA-Z]+", "");
 
-		}
+        }
 
-		return valor;
+        return valor;
 
-	}
+    }
 
-	public static String removeAccents(String str) {
+    public static String removeAccents(String str) {
 
-		str = Normalizer.normalize(str, Normalizer.Form.NFD);
-		str = str.replaceAll("[^\\p{ASCII}]", "");
+        str = Normalizer.normalize(str, Normalizer.Form.NFD);
+        str = str.replaceAll("[^\\p{ASCII}]", "");
 
-		return str;
+        return str;
 
-	}
+    }
 
-	public static ArrayList<String> preposicoes() {
+    public static ArrayList<String> preposicoes() {
 
         String[] array = {" da ", " de ", " do ", " a ", " e ", " o "};
         ArrayList<String> preposicoes = new ArrayList<>();
@@ -68,7 +68,7 @@ public class StringUtil {
             preposicoes.add(item);
         }
 
-	    return preposicoes;
+        return preposicoes;
     }
 
 }
