9023a59900e4e55091f0db445a869f38cb1bdafc
francis.moura <francis.lemaf@gmail.com>
2020-08-19 14:10:37 -0300

    Resolvendo conflitos corretamente

--- frontend/src/components/GridListagem.vue
+++ frontend/src/components/GridListagem.vue
@@ -27,13 +27,8 @@
 			template#text-align-center(v-slot:item.validadeEmAnos='{ item }')
 				span {{item.validadeEmAnos ? item.validadeEmAnos : ' ‒'}}
 
-<<<<<<< HEAD
 			template#teste(v-slot:item.finalidade='{ item }')
 				span {{item.finalidade}}
-=======
-			template#text-align-center(v-slot:item.finalidade='{ item }')
-				span {{item.finalidade.text}}
->>>>>>> ef293a27e3e85249e4e91ac05507409b69e79354
 
 			template(v-slot:item.ativo='{ item }')
 				span {{item.ativo ? 'Ativo' : 'Inativo'}}
