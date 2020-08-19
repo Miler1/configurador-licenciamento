44c4aed6d77598edb5eb1830e5205c62bfa092ec
kellyson.santos <kellyson.santos.lemaf@gmail.com>
2020-08-19 11:04:41 -0300

    TIPOLOGIA - Preparando frontend para listar as tipologias

---
+++ frontend/src/utils/dadosMockados/ListagemTipologiaHeader.js
@@ -0,0 +1,9 @@
+const HEADER = [
+	{ text: 'Nome', value: 'nome' },
+	{ text: 'Status', value: 'ativo', width: '145px'},
+	{ text: 'Ações', value: 'actions', sortable: false, align: 'center', width: '100px'},
+];
+
+export {
+	HEADER
+};
