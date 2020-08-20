34c22bca4e8d848398502453e770d79e2d00afe1
kellyson.santos <kellyson.santos.lemaf@gmail.com>
2020-08-20 11:12:14 -0300

    CODIGO - Alterando strings para single quote

--- frontend/src/utils/helpers/messages-utils.js
+++ frontend/src/utils/helpers/messages-utils.js
@@ -8,49 +8,49 @@ const ERROR_MESSAGES = {
 		cadastro: ER_BASE + 'cadastrar CNAE.',
 		listagem: ER_BASE + 'listar os CNAEs.',
 		editar: ER_BASE + 'editar o CNAE.',
-		ativar: ER_BASE + "ativar o CNAE. ",
-		desativar: ER_BASE + "desativar o CNAE. "
+		ativar: ER_BASE + 'ativar o CNAE. ',
+		desativar: ER_BASE + 'desativar o CNAE. '
 	},
 
 	licenca: {
-		cadastro: ER_BASE + "cadastrar licença. ",
+		cadastro: ER_BASE + 'cadastrar licença. ',
 		editar: ER_BASE + 'editar a licença. ',
-		ativar: ER_BASE + "ativar a licença. ",
+		ativar: ER_BASE + 'ativar a licença. ',
 		listagem: ER_BASE + 'listar as licenças.',
-		desativar: ER_BASE + "desativar a licença. "
+		desativar: ER_BASE + 'desativar a licença. '
 	},
 
 	tipologia: {
-		cadastro: ER_BASE + "cadastrar tipologia. ",
+		cadastro: ER_BASE + 'cadastrar tipologia. ',
 		listagem: ER_BASE + 'listar as tipologias. ',
 		editar: ER_BASE + 'editar a tipoloigia. ',
-		ativar: ER_BASE + "ativar a tipologia. ",
-		desativar: ER_BASE + "desativar a tipologia. ",
+		ativar: ER_BASE + 'ativar a tipologia. ',
+		desativar: ER_BASE + 'desativar a tipologia. ',
 	},
 
 	parametro: {
-		cadastro: ER_BASE + "cadastrar Parâmetro"
+		cadastro: ER_BASE + 'cadastrar Parâmetro'
 	}
 };
 
 const SUCCESS_MESSAGES = {
 
-	cadastro: "Sucesso! As informações foram salvas.",
-	editar: "Sucesso! As informações foram editadas.",
+	cadastro: 'Sucesso! As informações foram salvas.',
+	editar: 'Sucesso! As informações foram editadas.',
 
 	cnae: {
-		ativar: "Sucesso! O CNAE foi ativado.",
-		desativar: "Sucesso! O CNAE foi desativado.",
+		ativar: 'Sucesso! O CNAE foi ativado.',
+		desativar: 'Sucesso! O CNAE foi desativado.',
 	},
 
 	licenca: {
-		ativar: "Sucesso! A licença foi ativada.",
-		desativar: "Sucesso! A licença foi desativada."
+		ativar: 'Sucesso! A licença foi ativada.',
+		desativar: 'Sucesso! A licença foi desativada.'
 	},
 
 	tipologia: {
-		ativar: "Sucesso! A tipologia foi ativada.",
-		desativar: "Sucesso! A tipologia foi desativada."
+		ativar: 'Sucesso! A tipologia foi ativada.',
+		desativar: 'Sucesso! A tipologia foi desativada.'
 	}
 };
 
