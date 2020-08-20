e62957f030bf1e0cb3296f45f2b1ddd46cbbbe27
raydson.ferreira <raydson.ferreira.lemaf@gmail.com>
2020-08-20 10:10:16 -0300

    Criando DTO e repository do documento

---
+++ backend/src/main/java/com/configuradorlicenciamento/documento/repositories/DocumentoRepository.java
@@ -0,0 +1,10 @@
+package com.configuradorlicenciamento.documento.repositories;
+
+import com.configuradorlicenciamento.documento.models.Documento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DocumentoRepository extends JpaRepository<Documento, Integer>, JpaSpecificationExecutor<Documento> {
+}
