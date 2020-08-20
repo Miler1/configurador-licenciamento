2aec00aa7776638886e24302873e7cc3b0d48326
felipe.silva <felipe.ferreira.lemaf@gmail.com>
2020-08-20 13:42:28 -0300

    Implementa listagem de parâmetros na aba parâmetros

---
+++ frontend/src/utils/dadosMockados/ListagemParametroHeader.js
@@ -0,0 +1,13 @@
+const HEADER = [
+    
+	{ text: 'Código', value: 'codigo', width: '175px' },
+	{ text: 'Nome', value: 'nome' },
+	{ text: 'Casas decimais', value: 'casasDecimais', width: '145px'},
+	{ text: 'Status', value: 'ativo', width: '145px'},
+	
+	{ text: 'Ações', value: 'actions', sortable: false, align: 'center', width: '100px'},
+];
+
+export {
+	HEADER
+};
