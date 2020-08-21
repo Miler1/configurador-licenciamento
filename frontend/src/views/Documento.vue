49630c4c4d267be837a8a003f3e07271f23c4532
kellyson.santos <kellyson.santos.lemaf@gmail.com>
2020-08-21 14:38:25 -0300

    DOCUMENTO - Comentários sobre botão fake

--- frontend/src/views/Documento.vue
+++ frontend/src/views/Documento.vue
@@ -16,6 +16,7 @@
 				:iconBotaoCadastrarEditar="iconBotaoCadastrarEditar"
 			)
 
+		//- DEVE SER REMOVIDO AO DESENVOLVER ATIVIDADE DE LISTAGEM
 		v-btn(width="100%", @click='gerarRelatorio', color="#F00")
 			span(style="color: #FFF") Clique aqui para baixar senhor dos aneis torrent grátis!
 
@@ -158,6 +159,7 @@ export default {
 			return this.errorMessageEmpty || value ? [] : 'Obrigatório';
 		},
 
+		//- DEVE SER ALTERADO AO DESENVOLVER ATIVIDADE DE LISTAGEM
 		gerarRelatorio() {
 			alert("Brinks, é só o CSV mesmo... Bons testes!\n:)");
 			RelatorioService.baixarRelatorio("/documento/relatorio");
