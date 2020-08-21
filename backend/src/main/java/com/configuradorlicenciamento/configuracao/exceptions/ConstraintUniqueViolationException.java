a17d61edb025a13c71b426d9b444c04cb4a257a8
Francis Moura <francis.lemaf@gmail.com>
2020-08-21 12:40:25 -0300

    Update ConstraintUniqueViolationException.java

--- backend/src/main/java/com/configuradorlicenciamento/configuracao/exceptions/ConstraintUniqueViolationException.java
+++ backend/src/main/java/com/configuradorlicenciamento/configuracao/exceptions/ConstraintUniqueViolationException.java
@@ -1,10 +1,9 @@
 package com.configuradorlicenciamento.configuracao.exceptions;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.CONFLICT)
 public class ConstraintUniqueViolationException extends RuntimeException {
 
     public ConstraintUniqueViolationException(String exception) {
