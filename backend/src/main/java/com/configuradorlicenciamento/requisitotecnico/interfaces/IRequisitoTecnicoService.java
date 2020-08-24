583f145d35e296ba83f280e5f755d7de904202af
lucas <lucas.pellaquim.lemaf@gmail.com>
2020-08-24 15:52:04 -0300

    Gerar relatorios de Requisitos Tecnicos

--- backend/src/main/java/com/configuradorlicenciamento/requisitotecnico/interfaces/IRequisitoTecnicoService.java
+++ backend/src/main/java/com/configuradorlicenciamento/requisitotecnico/interfaces/IRequisitoTecnicoService.java
@@ -1,12 +1,17 @@
 package com.configuradorlicenciamento.requisitotecnico.interfaces;
 
 import com.configuradorlicenciamento.configuracao.utils.FiltroPesquisa;
+import com.configuradorlicenciamento.requisitotecnico.dtos.RequisitoTecnicoCsv;
 import com.configuradorlicenciamento.requisitotecnico.models.RequisitoTecnico;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 
 public interface IRequisitoTecnicoService {
 
     Page<RequisitoTecnico> listar(Pageable pageable, FiltroPesquisa filtro);
+
+    List<RequisitoTecnicoCsv> listarDocumentoParaCsv() throws Exception;
 }
