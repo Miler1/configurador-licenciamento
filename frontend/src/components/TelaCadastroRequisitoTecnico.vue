ddf53a6284735b68bdb1cd89e83bd60742c5a520
felipe.silva <felipe.ferreira.lemaf@gmail.com>
2020-08-28 09:00:39 -0300

    Correção no cadastro de requisitos técnicos

--- frontend/src/components/TelaCadastroRequisitoTecnico.vue
+++ frontend/src/components/TelaCadastroRequisitoTecnico.vue
@@ -354,8 +354,12 @@ export default {
 			);
 
 			this.clearRequisito();
-			this.cancelar();
+			this.redirectListagem();
 
+		},
+
+		redirectListagem() {
+			this.$router.push({name: 'RequisitosTecnicos'});
 		},
 
 		checkForm() {
