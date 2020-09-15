9ad54ba82e5c4db32fcac62a34ef407fe9ffb28d
Leonardo Maranha de Oliveira <leonardo.maranha.lemaf@gmail.com>
2020-09-15 13:40:01 -0300

    Corrige nome das colunas invertidas, porte e ppd

--- frontend/src/utils/dadosHeader/ListagemTaxaLicenciamentoInclusao.js
+++ frontend/src/utils/dadosHeader/ListagemTaxaLicenciamentoInclusao.js
@@ -1,6 +1,6 @@
 const HEADER = [
-	{ text: 'PPD', value: 'porteEmpreendimento.nome'},
-	{ text: 'Porte', value: 'potencialPoluidor.nome'},
+	{ text: 'Porte', value: 'porteEmpreendimento.nome'},
+	{ text: 'PPD', value: 'potencialPoluidor.nome'},
     { text: 'Tipo de licença', value: 'licenca.sigla'},
 	{ text: 'Valor (R$)', value: 'valor'},
 	{ text: 'Ações', value: 'actions', sortable: false, align: 'center', width: '100px'}
