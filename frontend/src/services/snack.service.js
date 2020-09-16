fdf7c760bd6cf99b3e76b6af38a628e8474cc8eb
kellyson.santos <kellyson.santos.lemaf@gmail.com>
2020-09-16 09:04:00 -0300

    236 - Ajustando ícone para a snackbar

--- frontend/src/services/snack.service.js
+++ frontend/src/services/snack.service.js
@@ -3,14 +3,37 @@ import store from '@/store/index';
 
 let types = {
 	ERROR: 'error',
+	INFO: 'info',
 	SUCCESS: 'success',
 	WARN: 'warning'
+};
+
+let iconDecider = type => {
+
+	switch(type){
+
+		case types.ERROR:
+			return 'mdi-alert';
+
+		case types.SUCCESS:
+			return 'mdi-check-circle';
+
+		case types.INFO:
+			return 'alert-circle';
+
+		case types.WARN:
+			return 'mdi-information';
+
+		default:
+			return;
+	}
+
 };
 
 export default {
 
 	alert: (mensagem, tipo = types.ERROR, timeout = '6000') => {
-		store.dispatch(SET_SNACKBAR, {color: tipo, text: mensagem, timeout: timeout});
+		store.dispatch(SET_SNACKBAR, {color: tipo, text: mensagem, timeout: timeout, icon: iconDecider(tipo)});
 	},
 
 	type: types
