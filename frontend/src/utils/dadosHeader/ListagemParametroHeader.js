1d000f6b7425aeb08b3f379e22679c0e6dddfc91
Leonardo Maranha de Oliveira <leonardo.maranha.lemaf@gmail.com>
2020-09-16 17:44:08 -0300

    Corrige quebra de label no grid

--- frontend/src/utils/dadosHeader/ListagemParametroHeader.js
+++ frontend/src/utils/dadosHeader/ListagemParametroHeader.js
@@ -2,7 +2,7 @@ const HEADER = [
     
 	{ text: 'Código', value: 'codigo', width: '175px' },
 	{ text: 'Descrição', value: 'nome' },
-	{ text: 'Casas decimais', value: 'casasDecimais', width: '145px'},
+	{ text: 'Casas decimais', value: 'casasDecimais', width: '150px'},
 	{ text: 'Status', value: 'ativo', width: '145px'},
 	{ text: 'Ações', value: 'actions', sortable: false, align: 'center', width: '100px'},
 ];
