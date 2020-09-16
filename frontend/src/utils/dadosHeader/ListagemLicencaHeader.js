e37d6ffe4c604af438a2e34ee5fe9e722c6e5b53
Leonardo Maranha de Oliveira <leonardo.maranha.lemaf@gmail.com>
2020-09-16 18:24:54 -0300

    Update frontend/src/utils/dadosHeader/ListagemLicencaHeader.js

--- frontend/src/utils/dadosHeader/ListagemLicencaHeader.js
+++ frontend/src/utils/dadosHeader/ListagemLicencaHeader.js
@@ -1,7 +1,7 @@
 const HEADER = [
 	{ text: 'Tipo', value: 'sigla', width: '175px'},
 	{ text: 'Nomenclatura', value: 'nome'},
-	{ text: 'Prazo de validade (anos)', value: 'validadeEmAnos'},
+	{ text: 'Prazo de validade (anos)', value: 'validadeEmAnos', width: '160px'},
 	{ text: 'Finalidade', value: 'finalidade'},
 	{ text: 'Status', value: 'ativo', width: '145px'},
 	{ text: 'Ações', value: 'actions', sortable: false, align: 'center', width: '100px'},
