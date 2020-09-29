97b584c0c86a659710d2e289148058ff9b53a32e
raydson.ferreira <raydson.ferreira.lemaf@gmail.com>
2020-09-29 10:50:19 -0300

    274 - Correção do filtro de cnaes vinculados e ativos para atividades dispensáveis

--- frontend/src/views/atividade/dispensavel/components/Cnaes.vue
+++ frontend/src/views/atividade/dispensavel/components/Cnaes.vue
@@ -392,7 +392,7 @@ export default {
 
 	created() {
 
-		atividadeCnaeService.buscarCnaesAtivos()
+		atividadeCnaeService.buscarCnaesAtivosNaoVinculados()
 			.then((response) => {
 
 				this.cnaes = response.data;
