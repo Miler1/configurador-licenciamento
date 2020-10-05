e1aff36d4a3ce6db21ee57e7d3d6cbaf6989bdd1
miler.boell <milerboell.zetta@ufla.br>
2020-10-05 13:56:19 -0300

    307 - Criação das steps das atividades licenciaveis

---
+++ frontend/src/views/atividade/licenciavel/components/Resumo.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+	#under-construction
+		v-icon mdi-account-hard-hat
+		h1 Página em desenvolvimento!
+</template>
+
+<style lang="less" scoped>
+
+#under-construction {
+	margin-top: 20%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	color: #9E9E9E;
+}
+.v-icon	{
+	font-size: 100px !important;
+}
+
+</style>
