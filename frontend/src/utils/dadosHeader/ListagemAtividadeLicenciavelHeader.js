c55f9e0104f3360ddd04971cba2527a70d568d55
raydson.ferreira <raydson.ferreira.lemaf@gmail.com>
2020-10-06 07:58:07 -0300

    correção de navegação

--- frontend/src/utils/dadosHeader/ListagemAtividadeLicenciavelHeader.js
+++ frontend/src/utils/dadosHeader/ListagemAtividadeLicenciavelHeader.js
@@ -2,7 +2,7 @@ const HEADER = [
 	{ text: 'Código', value: 'codigo', width: '155px'},
 	{ text: 'Nome', value: 'nome'},
 	{ text: 'Tipologia', value: 'tipologia.nome'},
-	{ text: 'Parâmetros', value: 'parametros'},
+	{ text: 'Parâmetros', value: 'parametros', width: '150px'},
 	{ text: 'Tipos de licença', value: 'tiposLicencas'},
 	{ text: 'Status', value: 'ativo', width: '145px'},
 	{ text: 'Ações', value: 'actions', sortable: false, align: 'center', width: '100px'},
