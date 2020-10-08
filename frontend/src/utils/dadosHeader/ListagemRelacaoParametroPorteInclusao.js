3d75dfa09a69a0afb6a4194967c264d4147efa31
Leonardo Maranha de Oliveira <leonardo.maranha.lemaf@gmail.com>
2020-10-08 14:14:39 -0300

    Corrige labels do header de relação parâmetro/porte

--- frontend/src/utils/dadosHeader/ListagemRelacaoParametroPorteInclusao.js
+++ frontend/src/utils/dadosHeader/ListagemRelacaoParametroPorteInclusao.js
@@ -1,12 +1,12 @@
 const HEADER = [
-	{ text: 'Parametro 1', value: 'parametro1.codigo', sortable: false},
+	{ text: 'Parâmetro 1', value: 'parametro1.codigo', sortable: false},
 	{ text: 'Valor mínimo', value: 'valorMinimoParametro1', sortable: false},
 	{ text: 'Valor máximo', value: 'valorMaximoParametro1', sortable: false},
-	{ text: 'Parametro 2', value: 'parametro2.codigo', sortable: false},
+	{ text: 'Parâmetro 2', value: 'parametro2.codigo', sortable: false},
 	{ text: 'Valor mínimo', value: 'valorMinimoParametro2', sortable: false},
 	{ text: 'Valor máximo', value: 'valorMaximoParametro2', sortable: false},
     { text: 'Porte', value: 'porte', width: '150px', sortable: false},
-	{ text: 'Licenciamento Municipal', value: 'licenciamentoMunicipal', sortable: false, align: 'center', width: '150px'},
+	{ text: 'Licenciamento municipal', value: 'licenciamentoMunicipal', sortable: false, align: 'center', width: '150px'},
 	{ text: 'Repasse para outro órgão', value: 'repasseOutroOrgao', sortable: false, align: 'center', width: '150px'}
 ];
 
