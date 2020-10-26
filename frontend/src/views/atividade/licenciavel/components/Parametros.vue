bbd5c3b7b01bbc81de99ae50385d73ca7ab474bf
Leonardo Maranha de Oliveira <leonardo.maranha.lemaf@gmail.com>
2020-10-26 14:30:24 -0300

    Altera mensagem informativa da descrição da unidade do parâmetro

--- frontend/src/views/atividade/licenciavel/components/Parametros.vue
+++ frontend/src/views/atividade/licenciavel/components/Parametros.vue
@@ -316,7 +316,7 @@ export default {
 			placeholderSelect2: 'Selecione o parâmetro 2',
 			placeholderDescricao1: 'Ex.: Metros cúbicos/dia (m³/d)',
 			placeholderDescricao2: 'Ex.: Produção em toneladas por mês',
-			labelTooltipDescricao: 'Esta é a descrição do campo parâmetro que será exibida para o solicitante ao informar o valor.',
+			labelTooltipDescricao: 'Esta é a descrição do campo parâmetro que será exibida para o solicitante ao informar o valor. Caso não informado, será exibida a própria descição do parâmetro.',
 
 			tituloListagem: "Listagem de relações parâmetros / portes adicionadas",
 			labelNoData: 'Não existem relações parâmetros / portes adicionadas.',
