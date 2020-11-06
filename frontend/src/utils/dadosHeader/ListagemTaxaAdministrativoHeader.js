b15dc923ec6ce8332d148f944b36cdcfd16dd2e3
Leonardo Maranha de Oliveira <leonardo.maranha.lemaf@gmail.com>
2020-11-06 14:41:27 -0200

    Padroniza nomenclaturas das atividades no menu taxa administrativa

--- frontend/src/utils/dadosHeader/ListagemTaxaAdministrativoHeader.js
+++ frontend/src/utils/dadosHeader/ListagemTaxaAdministrativoHeader.js
@@ -2,7 +2,7 @@ const HEADER = [
 	{ text: 'Ano', value: 'ano', width: '125px'},
 	{ text: 'Isento', value: 'isento', width: '125px'},
 	{ text: 'Valor (R$)', value: 'valor', width: '150px'},
-	{ text: 'Atividade dispensável', value: 'atividadeDispensavel', width: 'auto' },
+	{ text: 'CNAE dispensável', value: 'atividadeDispensavel', width: 'auto' },
 	{ text: 'Atividade licenciável', value: 'atividadeLicenciavel', width: 'auto' },
 	{ text: 'Status', value: 'ativo', width: '100px' },
 	{ text: 'Ações', value: 'actions', sortable: false, align: 'center', width: '150px'},
